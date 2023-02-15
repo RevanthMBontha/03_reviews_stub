771976fe4a109ffc27ed56b10daa0c713d8ad038
Revanth M. Bontha <69347893+RevanthMBontha@users.noreply.github.com>
2023-02-15 10:34:52 +0530

    change review to fetch data from external URL

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
-import people from '../data';
+import React, { useState, useEffect } from 'react';
+// import people from '../data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
-const Review = () => {
-  const [index, setIndex] = useState(0);
-  const [canReadMore, setCanReadMore] = useState(true);
+const url =
+  'https://api-for-basic-projects.netlify.app/reviews-project/reviews_data.json';
 
-  const { name, job, image, text } = people[index];
+const Review = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [people, setPeople] = useState([]);
+  const [canReadMore, setCanReadMore] = useState(true);
 
   const handlePrev = () => {
     index > 0 ? setIndex(index - 1) : setIndex(people.length - 1);
@@ -24,36 +27,56 @@ const Review = () => {
     setCanReadMore(true);
   };
 
-  return (
-    <article className="review">
-      <div className="img-container">
-        <img src={image} alt={name} className="person-img" />
-        <span className="quote-icon">
-          <FaQuoteRight />
-        </span>
-      </div>
+  const fetchReviews = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setPeople(data);
+    setIsLoading(false);
+  };
 
-      <h4 className="author">{name}</h4>
-      <p className="job">{job}</p>
-      <p className="info">
-        {canReadMore ? `${text.substring(0, 100)}...` : text}
-        <button className="btn" onClick={() => setCanReadMore(!canReadMore)}>
-          {canReadMore ? 'read more' : 'show less'}
-        </button>
-      </p>
-      <div className="button-container">
-        <button className="prev-btn" onClick={handlePrev}>
-          <FaChevronLeft />
-        </button>
-        <button className="next-btn" onClick={handleNext}>
-          <FaChevronRight />
-        </button>
-        <button className="random-btn" onClick={handleRandom}>
-          surprise me!
-        </button>
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="review">
+        <h3>loading...</h3>
       </div>
-    </article>
-  );
+    );
+  } else {
+    const { name, image, text, job } = people[index];
+    return (
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">
+          {canReadMore ? `${text.substring(0, 100)}...` : text}
+          <button className="btn" onClick={() => setCanReadMore(!canReadMore)}>
+            {canReadMore ? 'read more' : 'show less'}
+          </button>
+        </p>
+        <div className="button-container">
+          <button className="prev-btn" onClick={handlePrev}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={handleNext}>
+            <FaChevronRight />
+          </button>
+          <button className="random-btn" onClick={handleRandom}>
+            surprise me!
+          </button>
+        </div>
+      </article>
+    );
+  }
 };
 
 export default Review;
